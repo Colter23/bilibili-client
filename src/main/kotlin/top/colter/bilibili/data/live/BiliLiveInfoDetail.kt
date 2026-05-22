cda7eb96efa73e94861f6772006e42c91921074b
Colter23 <colter_null@163.com>
2026-05-22 21:40:23 +0800

    update

---
+++ src/main/kotlin/top/colter/bilibili/data/live/BiliLiveInfoDetail.kt
@@ -0,0 +1,44 @@
+package top.colter.bilibili.data.live
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import top.colter.bilibili.data.LazyImage
+
+/**
+ *
+ */
+@Serializable
+public data class BiliLiveInfoDetail(
+    @SerialName("uid")
+    override val uid: Long,
+    @SerialName("room_id")
+    override val roomId: Long,
+    @SerialName("short_id")
+    val shortId: Long,
+    @SerialName("attention")
+    val attention: Int,
+    @SerialName("online")
+    val online: Int,
+    @SerialName("live_status")
+    override val status: LiveStatus,
+    @SerialName("parent_area_id")
+    val parentAreaId: Int,
+    @SerialName("parent_area_name")
+    val parentAreaName: String,
+    @SerialName("area_id")
+    val areaId: Int,
+    @SerialName("area_name")
+    val areaName: String,
+    @SerialName("background")
+    val background: String,
+    @SerialName("title")
+    override val title: String,
+    @SerialName("user_cover")
+    override val cover: LazyImage,
+    @SerialName("keyframe")
+    val keyframe: String,
+    @SerialName("live_time")
+    val liveTime: String,
+    @SerialName("up_session")
+    val upSession: String,
+) : BiliLive
